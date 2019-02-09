2e3030cb0f136a897999e2a89939962f85984f5f
Coastal <gavinpersonal9@gmail.com>
2019-02-09 00:21:37 -0600

    Create IslandLevel.java

---
+++ src/main/java/co/matrixdevelopment/solidskies/island/IslandLevel.java
@@ -0,0 +1,9 @@
+package co.matrixdevelopment.solidskies.island;
+
+//import co.matrixdevelopment.solidskies.island.SSIsland;
+//import co.matrixdevelopment.solidskies.filehandler.FileManager;
+//import co.matrixdevelopment.solidskies.filehandler.Messages;
+
+public class IslandLevel {
+    //TODO: work on level system next week
+}
